f2f902fb9daff9299e06ebf54b90b79d1a908d1c
eakantak <eakantak@mix.wvu.edu>
2020-03-19 18:20:32 -0400

    reading sans map

--- homework/final-wip/script.js
+++ homework/final-wip/script.js
@@ -1,173 +1,81 @@
-var promises = [
-    d3.csv("./bfro_reports_geocoded.csv",parseCSV), 
-    d3.json("./usa.json")
-];
+d3.csv("./bfro_reports_geocoded.csv", function(data) {
+    
+    console.log(data);
 
-Promise.all(promises).then(function(data) {
+    var width = document.querySelector("#chart").clientWidth;
+    var height = document.querySelector("#chart").clientHeight;
+    var margin = {top: 50, left: 150, right: 50, bottom: 150};
 
+    var svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+   
+    var filtered_data = data.filter(function(d) {
+        return d.state == "California";
+    });
 
-var bigfootData = data[0];
+    console.log(filtered_data);
 
-var usa = data[1];
+    /*
 
-console.log(data)
+    var lifeExp = {
+        min: d3.min(filtered_data, function(d) { return +d.lifeExp; }),
+        max: d3.max(filtered_data, function(d) { return +d.lifeExp; })
+    };
 
-var width = document.querySelector("#chart").clientWidth;
-var height = document.querySelector("#chart").clientHeight;
+    var gdpPercap = {
+        min: d3.min(filtered_data, function(d) { return +d.gdpPercap; }),
+        max: d3.max(filtered_data, function(d) { return +d.gdpPercap; })
+    };
 
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var pop = {
+        min: d3.min(filtered_data, function(d) { return +d.pop; }),
+        max: d3.max(filtered_data, function(d) { return +d.pop; })
+    };
 
+    var xScale = d3.scaleLinear()
+        .domain([lifeExp.min, lifeExp.max])
+        .range([margin.left, width-margin.right]);
 
-var projection = d3.geoAlbersUsa()
-    .translate([width/2, height/2])
-    .scale(100);
+    var yScale = d3.scaleLinear()
+        .domain([gdpPercap.min, gdpPercap.max])
+        .range([height-margin.bottom, margin.top]);
 
+    var rScale = d3.scaleSqrt()
+        .domain([pop.min, pop.max])
+        .range([3, 25]);
 
-var path = d3.geoPath().projection(projection);
+    var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
+    var xAxis = svg.append("g")
+        .attr("transform", `translate(0,${height-margin.bottom})`)
+        .call(d3.axisBottom().scale(xScale));
 
-svg.selectAll("path")
-    .data(usa.features)
-    .enter()
-    .append("path")
-        .attr("state")
-        .attr("d", path);
+    var yAxis = svg.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft().scale(yScale));
+    
+    var points = svg.selectAll("circle")
+        .data(filtered_data)
+        .enter()
+        .append("circle")
+            .attr("cx", function(d) { return xScale(d.lifeExp); })
+            .attr("cy", function(d) { return yScale(d.gdpPercap); })
+            .attr("r", function(d) { return rScale(d.pop); })
+            .attr("fill", function(d) { return colorScale(d.continent); })
 
-bigfootData = bigfootData.sort(function(a,b) { return a.date - b.date; });
+    var xAxisLabel = svg.append("text")
+        .attr("class", "axisLabel")
+        .attr("x", width/2)
+        .attr("y", height - margin.bottom/2)
+        .text("Life Expectancy");
 
+    var yAxisLabel = svg.append("text")
+        .attr("class","axisLabel")
+        .attr("transform","rotate(-90)")
+        .attr("x", -height/2)
+        .attr("y", margin.left/2)
+        .text("GDP Per Capita");
 
-
-
-var slider = d3.select("#selectDate");
-
-slider
-        .property("min", bigfootData[0].date)
-        .property("max", [7.1])
-        .property("value", bigfootData[bigfootData.length-1].date)
-        .property("tickValues",[1,3,5,7,10]);
-
-
-var selectedDate = slider.property("value");
-
-
-var dateLabel = svg.append("text")
-        .attr("class", "dateLabel")
-        .attr("x", 600)
-        .attr("y", height - 90)
-        .text(selectedDate)
-        .text("Date");
-
-var rScale = d3.scaleSqrt()
-        .domain([1, 125])
-        .range([0,475]);
-
-
-function updateMap(date) {
-
-        var filtered_data = bigfootData.filter(function(d) {
-            return d.date == date;
-        });
-
-var date = svg.selectAll("circle")
-            .data(filtered_data, function(d) { return d.state; })
-            .data(filtered_data, function(d) { return d.county; });
-
-
-    date.enter().append("circle")
-            .attr("cx", function(d) {
-                var proj = projection([d.long, d.lat]);
-                return proj[0];
-            }).attr("cy", function(d) {
-                var proj = projection([d.long, d.lat]);
-                return proj[1];
-            }).attr("r", 0)
-            .attr("opacity", 0.5)
-            .attr("fill", "white")
-        
-    .merge(date)
-            .transition()
-            .duration(800)
-            .attr("cx", function(d) {
-                var proj = projection([d.long, d.lat]);
-                return proj[0];
-            }).attr("cy", function(d) {
-                var proj = projection([d.long, d.lat]);
-                return proj[1];
-            }).attr("r", function(d) { return rScale(d.date); })
-            .attr("opacity", 0.5)
-            .attr("fill", "red");
-
-    date.exit()
-            .transition()
-            .duration(500)
-            .attr("r", 0)
-            .remove();
-
-dateLabel.text(date);
-
-svg.selectAll("circle")
-        .on("mouseover", function(d) {
-            var cx = +d3.select(this).attr("cx") + 100;
-            var cy = +d3.select(this).attr("cy") + 300;
-
-            tooltip.style("visibility","visible")
-                .style("left", cx + "px")
-                .style("top", cy + "px")
-                .html(d.state + "<br>" + d.date.toLocaleDateString("en-US") + "<br>" + d.county);
-
-            svg.selectAll("circle")
-                .attr("opacity",0.5);
-
-            d3.select(this)
-                .attr("opacity",0.7);
-
-        }).on("mouseout", function() {
-            tooltip.style("visibility","hidden");
-
-            svg.selectAll("circle")
-                .attr("opacity",0.3);
-            })
-
-    }
-
-updateMap(selectedDate);
-
-
-slider.on("input", function() {
-    var date = this.value;
-
-
-    selectedDate = date;
-    updateMap(selectedDate);
-
-    })
-
-
-var tooltip = d3.select("#chart")
-    .append("div")
-    .attr("class","tooltip");
-
-
-
-
-
-});
-
-function parseCSV(data) {
-    var d = {};
-    d.state = data.state;
-    d.lat = +data.lat;
-    d.long = +data.long;
-    d.date = +data["Date"];
-    d.county = +data.county;
-    d.date = new Date(data.date);
-
-    return d;
-
-
-
-
-}
+});*/})
