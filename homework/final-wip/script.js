e0671a398d90a7df2d295d103535b3a777c141a4
eakantak <eakantak@mix.wvu.edu>
2020-03-26 16:28:27 -0400

    updates

--- homework/final-wip/script.js
+++ homework/final-wip/script.js
@@ -1,174 +1,170 @@
-
 var promises = [
-    d3.csv("./data/bfro_reports_geocoded.csv"), 
+    d3.csv("./data/bfro_reports_geocoded.csv",parseCSV), 
     d3.json("./geojson/usa.json")
 ];
 
 Promise.all(promises).then(function(data) {
 
+    console.log(data);
 
-    var quakeData = data[0];
-    
+    var bigfootData = data[0];
+
     var usa = data[1];
-    
-    console.log(data)
-    
-    var width = document.querySelector("#chart1").clientWidth;
-    var height = document.querySelector("#chart1").clientHeight;
-    
-    var svg = d3.select("#chart1")
+
+    var width = document.querySelector("#chart").clientWidth;
+    var height = document.querySelector("#chart").clientHeight;
+    var svg = d3.select("#chart")
         .append("svg")
         .attr("width", width)
         .attr("height", height);
-    
-    
-    var projection = d3.geoMercator()
+
+
+    var projection = d3.geoAlbers()
         .translate([width/2, height/2])
-        .scale(100);
-    
-    
+        .scale(1000);
+
+
     var path = d3.geoPath().projection(projection);
-    
-    
+
+
     svg.selectAll("path")
         .data(usa.features)
+        .enter()
         .append("path")
-            .attr("class","place")
+            .attr("class","state")
             .attr("d", path);
-    
-    quakeData = quakeData.sort(function(a,b) { return a.magnitude - b.magnitude; });
-    
-    
-    
-    
-    var slider = d3.select("#selectMagnitude");
-    
+
+    bigfootData = bigfootData.sort(function(a,b) { return a.year - b.year; });
+
+
+    var slider = d3.select("#selectYear");
+
     slider
-            .property("min", quakeData[0].magnitude)
-            .property("max", [7.1])
-            .property("value", quakeData[quakeData.length-1].magnitude)
-            .property("tickValues",[1,3,5,7,10]);
+        .property("min", bigfootData[0].year)
+        .property("max", bigfootData[bigfootData.length-1].year)
+        .property("value", bigfootData[bigfootData.length-1].year);
+
+    var selectedYear = slider.property("value");
+
+
+    var yearLabel = svg.append("text")
+        .attr("class", "yearLabel")
+        .attr("x", 25)
+        .attr("y", height - 100)
+        .text(selectedYear);
     
-    
-    var selectedMagnitude = slider.property("value");
-    
-    
-    var magnitudeLabel = svg.append("text")
-            .attr("class", "magnitudeLabel")
-            .attr("x", 600)
-            .attr("y", height - 90)
-            .text(selectedMagnitude)
-            .text("Magnitude");
-    
+
     var rScale = d3.scaleSqrt()
-            .domain([1, 125])
-            .range([0,475]);
-    
-    
-    function updateMap(magnitude) {
-    
-            var filtered_data = quakeData.filter(function(d) {
-                return d.magnitude == magnitude;
-            });
-    
-    var mag = svg.selectAll("circle")
-                .data(filtered_data, function(d) { return d.place; })
-                .data(filtered_data, function(d) { return d.Depth; });
-    
-    
-        mag.enter().append("circle")
-                .attr("cx", function(d) {
-                    var proj = projection([d.longitude, d.latitude]);
-                    return proj[0];
-                }).attr("cy", function(d) {
-                    var proj = projection([d.longitude, d.latitude]);
-                    return proj[1];
-                }).attr("r", 0)
-                .attr("opacity", 0.5)
-                .attr("fill", "white")
+        .domain([0, 50])
+        .range([0,25]);
+
+
+    function updateMap(year) {
+
+        var filtered_data = bigfootData.filter(function(d) {
+            return d.year == year;
+        });
+
+        var c = svg.selectAll("circle")
+            .data(filtered_data, function(d) { return d.observed; });
+
+        c.enter().append("circle")
+            .attr("cx", function(d) {
+                var proj = projection([d.longitude, d.latitude]);
+                return proj[0];
+            }).attr("cy", function(d) {
+                var proj = projection([d.longitude, d.latitude]);
+                return proj[1];            
+            }).attr("r", 0)
+            .attr("opacity", 0.7)
+            .attr("fill", "#CC0000")
+        .merge(c)
+            .transition()
+            .duration(1000)
+            .attr("cx", function(d) {
+                var proj = projection([d.longitude, d.latitude]);
+                return proj[0];
+            }).attr("cy", function(d) {
+                var proj = projection([d.longitude, d.latitude]);
+                return proj[1];            
+            }).attr("r", 0.5)
+            .attr("opacity", 0.7)
+            .attr("fill", "#CC0000");    
             
-        .merge(mag)
-                .transition()
-                .duration(800)
-                .attr("cx", function(d) {
-                    var proj = projection([d.longitude, d.latitude]);
-                    return proj[0];
-                }).attr("cy", function(d) {
-                    var proj = projection([d.longitude, d.latitude]);
-                    return proj[1];
-                }).attr("r", function(d) { return rScale(d.magnitude); })
-                .attr("opacity", 0.5)
-                .attr("fill", "red");
-    
-        mag.exit()
-                .transition()
-                .duration(500)
-                .attr("r", 0)
-                .remove();
-    
-    magnitudeLabel.text(magnitude);
-    
-    svg.selectAll("circle")
+        c.exit()
+            .transition()
+            .duration(1000)
+            .attr("r", 0)
+            .remove();
+
+        yearLabel.text(year);
+
+        svg.selectAll("circle")
             .on("mouseover", function(d) {
-                var cx = +d3.select(this).attr("cx") + 100;
-                var cy = +d3.select(this).attr("cy") + 300;
-    
+                var cx = +d3.select(this).attr("cx") + 15;
+                var cy = +d3.select(this).attr("cy") - 15;
+
                 tooltip.style("visibility","visible")
                     .style("left", cx + "px")
                     .style("top", cy + "px")
-                    .html(d.place + "<br>" + d.date.toLocaleDateString("en-US") + "<br>" + d.Depth);
-    
+                    .html(d.location + "<br>" + d.date.toLocaleDateString("en-US"));
+
                 svg.selectAll("circle")
-                    .attr("opacity",0.5);
-    
+                    .attr("opacity",0.2);
+
                 d3.select(this)
                     .attr("opacity",0.7);
-    
+
             }).on("mouseout", function() {
                 tooltip.style("visibility","hidden");
-    
+
                 svg.selectAll("circle")
-                    .attr("opacity",0.3);
-                })
+                    .attr("opacity",0.7);
+            })
+
+    }
+
+
+    updateMap(selectedYear);
+
     
-        }
-    
-    updateMap(selectedMagnitude);
-    
-    
+
     slider.on("input", function() {
-        var magnitude = this.value;
+        var year = this.value;
+        console.log(year);
+
+        selectedYear = year;
+        updateMap(selectedYear);
+
+    })
     
-    
-        selectedMagnitude = magnitude;
-        updateMap(selectedMagnitude);
-    
-        })
-    
-    
-    var tooltip = d3.select("#chart1")
+
+
+
+    var tooltip = d3.select("#chart")
         .append("div")
         .attr("class","tooltip");
     
-    
-    
-    
-    
-    });
-    
-    function parseCSV(data) {
-        var d = {};
-        d.place = data.place;
-        d.latitude = +data.Latitude;
-        d.longitude = +data.Longitude;
-        d.magnitude = +data["Magnitude"];
-        d.Depth = +data.Depth;
-        d.date = new Date(data.Date);
-    
-        return d;
-    
-    
-    
-    
-    }
-    
+
+
+
+
+});
+
+
+
+function parseCSV(data) {
+    var d = {};
+    d.observed = data.observed;
+    d.county = data.county;
+    d.latitude = +data.latitude;
+    d.longitude = +data.longitude;
+    d.season = +data.season["Season"]; 
+    d.date = new Date(data.date);
+    d.year = d.date.getFullYear();
+
+    return d;
+
+}
+
+
